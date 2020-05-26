a7e6034714e1ac3bd642d74c65a73c185b429350
yichen <1097179511@qq.com>
2020-05-26 20:25:30 +0800

    disenable ctrl+q/s

--- kilo.c
+++ kilo.c
@@ -13,6 +13,7 @@ void enableRawMode(){
   tcgetattr(STDIN_FILENO,&orig_termios);
   atexit(disableRawMode);
   struct termios raw = orig_termios;
+  raw.c_iflag &= ~(IXON);
   raw.c_lflag &= ~(ECHO | ICANON | ISIG);
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
