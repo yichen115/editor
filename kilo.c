8f8dfad2539d4056e9eaaa50ac0e3cc7e125aa6f
yichen <1097179511@qq.com>
2020-06-02 10:25:18 +0800

    清空屏幕啥的

--- kilo.c
+++ kilo.c
@@ -17,6 +17,8 @@ struct termios orig_termios;
 /*** terminal ***/
 
 void die(const char *s){
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
   perror(s);
   exit(1);
 }
@@ -53,6 +55,7 @@ char editorRedayKey(){
 
 void editorRefreshScreen(){
   write(STDOUT_FILENO,"\x1b[2J",4);
+  write(STDOUT_FILENO,"\x1b[H",3);
 }
 
 /*** input ***/
@@ -61,6 +64,8 @@ void editorProcessKeypress(){
   char c = editorRedayKey();
   switch(c){
     case CTRL_KEY('q'):
+      write(STDOUT_FILENO, "\x1b[2J", 4);
+      write(STDOUT_FILENO, "\x1b[H", 3);
       exit(0);
       break;
   }
